8ebf34232cbcad0d9d69ab9efe700437e02b252c
Den Kochetkov <d@zdp.su>
2021-06-24 14:00:28 +0300

    Fonts changed

--- src/lib/Footer/index.svelte
+++ src/lib/Footer/index.svelte
@@ -3,12 +3,40 @@
 </script>
 
 <footer>
-  footer
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <div class="logo">
+          La Bagon<sup>®</sup>
+        </div>
+      </div>
+    </div>
+  </div>
 </footer>
 
 <style>
   footer {
     position: sticky;
     top: 100%;
+    background: #fff;
+    padding: 50px 0;
+  }
+  .logo {
+    text-align: center;
+    white-space: nowrap;
+    color: #00406F;
+    font-family: serif;
+    font-weight: 600;
+    font-style: italic;
+    font-size: 36px;
+    letter-spacing: .05em;
+    text-decoration: none;
+    margin-bottom: 32px;
+  }
+  sup {
+    font-style: normal;
+    font-size: 16px;
+    display: inline-block;
+    transform: translateY(-4px);
   }
 </style>
