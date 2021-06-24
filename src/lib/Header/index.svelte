bfd3f03b330d20e02c0ef3b040c9f27d41b8a5a3
Den Kochetkov <d@zdp.su>
2021-06-24 13:38:41 +0300

    Header added

--- src/lib/Header/index.svelte
+++ src/lib/Header/index.svelte
@@ -1,12 +1,260 @@
 <script>
 	import { page } from '$app/stores';
+  import heart from './heart.svg';
+  import bag from './bag.svg';
+  import { yearFrom } from '$lib/helpers';
+  import { itemsInCart } from '$lib/stores';
+
+  let isOpen = false;
+
+  function toggleOpen() {
+    isOpen = !isOpen;
+  }
+
 </script>
 
 <!-- <li class:active={$page.path === '/'}><a sveltekit:prefetch href="/">Home</a></li> -->
 <header>
-  header
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <div class="nav">
+          <button on:click|preventDefault={toggleOpen}>
+            <span class:open={isOpen}></span>
+            <span class:open={isOpen}></span>
+            <span class:open={isOpen}></span>
+          </button>
+          <a href="/" class="logo">
+            La Bagon<sup>®</sup>
+          </a>
+          <a href="/favorites" class="favorites">
+            <img src={heart} alt="">
+          </a>
+          <a href="/cart" class="cart">
+            <img src={bag} alt="">
+            <div class="count">
+              {$itemsInCart.length}
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
 </header>
 
-<style>
+<div class="bg" class:open={isOpen}></div>
 
+<nav class:open={isOpen}>
+  <ul>
+    <li>
+      <a href="/">Home</a>
+    </li>
+    <li>
+      <a href="/">Shop</a>
+    </li>
+    <li>
+      <a href="/">About</a>
+    </li>
+    <li>
+      <a href="/">Boutiques</a>
+    </li>
+    <li>
+      <a href="/">Contacts</a>
+    </li>
+    <li class="light lined">
+      <a href="/">Login</a>
+    </li>
+    <li class="light">
+      <a href="/">Register</a>
+    </li>
+  </ul>
+  <a href="/" class="copy">
+    &copy; {yearFrom(2016)}, La Bagon
+  </a>
+</nav>
+
+<style>
+  header {
+    background: rgba(255,255,255,.9);
+    padding: 8px 0;
+    z-index: 10;
+    position: sticky;
+    top: 0;
+    -webkit-backdrop-filter: blur(2px);
+    backdrop-filter: blur(2px);
+    box-shadow: 0px 0px 8px 0px rgba(34, 60, 80, 0.2);
+  }
+  .nav {
+    display: grid;
+    grid-template-columns: min-content 1fr min-content min-content;
+    gap: 16px;
+    align-items: center;
+  }
+  button {
+    display: grid;
+    grid-template-columns: 1fr;
+    background: none;
+    border: none;
+    margin: 0;
+    padding: 0;
+    width: 32px;
+    gap: 4px;
+    cursor: pointer;
+  }
+  button span {
+    display: block;
+    width: 100%;
+    height: 4px;
+    background: #59b6d0;
+    transition: .25s ease-in-out;
+    margin: 0 auto;
+  }
+  button span:nth-of-type(1).open {
+    transform: translate(0%, 200%) rotate(45deg);
+  }
+  button span:nth-of-type(2).open {
+    width: 0;
+  }
+  button span:nth-of-type(3).open {
+    transform: translate(0%, -200%) rotate(-45deg);
+  }
+  .logo {
+    text-align: center;
+    white-space: nowrap;
+    color: #00406F;
+    font-family: 'Josefin Sans', serif;
+    font-weight: 500;
+    font-style: italic;
+    font-size: 32px;
+    letter-spacing: .05em;
+    text-decoration: none;
+  }
+  sup {
+    font-style: normal;
+    font-size: 14px;
+    display: inline-block;
+    transform: translateY(-4px);
+  }
+  .nav a {
+    display: block;
+    z-index: 1;
+    position: relative;
+  }
+  .favorites {
+    width: 18px;
+    height: 18px;
+    margin-top: 4px;
+  }
+  .favorites img,
+  .cart img {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+    object-position: center;
+  }
+  .cart {
+    width: 38px;
+    height: 38px;
+    padding: 4px;
+    text-decoration: none;
+  }
+  .count {
+    position: absolute;
+    right: 0;
+    top: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 16px;
+    min-width: 16px;
+    color: #fff;
+    text-align: center;
+    font-size: 12px;
+    padding: 4px;
+    border-radius: 8px;
+    background: #59b6d0;
+    pointer-events: none;
+  }
+  .bg {
+    pointer-events: none;
+    position: fixed;
+    left: 0;
+    top: 0;
+    background: rgba(0,0,0,0);
+    z-index: 1;
+    width: 100%;
+    height: 100vh;
+    transition: .25s ease-in-out;
+  }
+  .bg.open {
+    background: rgba(0,0,0,.5);
+    -webkit-backdrop-filter: blur(2px);
+    backdrop-filter: blur(2px);
+  }
+  nav {
+    position: fixed;
+    top: 0;
+    left: 0;
+    background: #fff;
+    transition: .25s ease-in-out;
+    height: 100vh;
+    margin-right: auto;
+    width: auto;
+    z-index: 1;
+    box-shadow: 0px 0px 8px 0px rgba(34, 60, 80, 0.2);
+    padding: 96px 16px 16px 16px;
+    white-space: nowrap;
+    min-width: 75%;
+    pointer-events: none;
+    transform: translateX(-110%);
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    letter-spacing: .05em;
+  }
+  nav.open {
+    pointer-events: all;
+    transform: translateX(0);
+  }
+  nav a {
+    text-transform: uppercase;
+    text-decoration: none;
+    color: #00406F;
+    font-size: 18px;
+    font-weight: 600;
+  }
+  .light {
+    position: relative;
+  }
+  .lined {
+    padding-top: 32px;
+  }
+  .lined::before {
+    content: '';
+    display: block;
+    width: 100%;
+    height: 2px;
+    background: #59b6d0;
+    position: absolute;
+    left: 0;
+    top: 8px;
+  }
+  .light a {
+    color: #59b6d0;
+  }
+  ul {
+    display: grid;
+    grid-template-columns: 1fr;
+    gap: 16px;
+  }
+  .copy {
+    margin-left: auto;
+    text-align: right;
+    font-style: italic;
+    text-transform: none;
+    font-weight: 500;
+    font-family: 'Josefin Sans', serif;
+    font-size: 16px;
+
+  }
 </style>
---
+++ src/lib/helpers.js
@@ -0,0 +1,115 @@
+export const toSlug = function (str) {
+	str = str.toLowerCase().replace(/<.+>/, ' ').replace(/\s+/, ' ');
+	let c = {
+		'а':'a', 'б':'b', 'в':'v', 'г':'g', 'д':'d', 'е':'e', 'ё':'jo', 'ж':'zh', 'з':'z', 'и':'i', 'й':'j', 'к':'k', 'л':'l', 'м':'m', 'н':'n', 'о':'o', 'п':'p', 'р':'r', 'с':'s', 'т':'t', 'у':'u', 'ф':'f', 'х':'h', 'ц':'c', 'ч':'ch', 'ш':'sh', 'щ':'shch', 'ъ':'', 'ы':'y', 'ь':'', 'э':'e', 'ю':'ju', 'я':'ja', ' ':'-', ';':'', ':':'', ',':'', '—':'-', '–':'-', '.':'', '«':'', '»':'', '"':'', "'":'', '@':'', '(':'', ')':'', '№':'n', '/':'-', '\\':'', '\?':'', '%':''
+	};
+	let newStr = new String();
+	for (let i = 0; i < str.length; i++) {
+		let ch = str.charAt(i);
+		newStr += ch in c ? c[ch] : ch;
+	};
+	return newStr;
+};
+
+export const imgFiles = function (filename) {
+  return {
+    full: filename,
+    large: filename.replace(/.(?=[^.]*$)/, '@large.'),
+    medium: filename.replace(/.(?=[^.]*$)/, '@medium.'),
+    small: filename.replace(/.(?=[^.]*$)/, '@small.'),
+    thumb: filename.replace(/.(?=[^.]*$)/, '@thumb.'),
+  }
+};
+
+export const formatDate = function (date) {
+  let monthNames = ["января", "февраля", "марта", "апреля", "мая", "июня", "июля", "августа", "сентября", "октября", "ноября", "декабря"];
+  date = new Date(date);
+  let d = date.getDate();
+  let m = monthNames[date.getMonth()];
+  let y = date.getFullYear();
+  let h = ('0' + date.getHours()).slice(-2);
+  let min = ('0' + date.getMinutes()).slice(-2);
+  return {
+    date: d + ' ' + m + ' ' + y + ' года',
+    time: h + ':' + min
+  };
+};
+
+// Dispatch event on click outside of node
+export function clickOutside(node) {
+  const handleClick = event => {
+    if (node && !node.contains(event.target) && !event.defaultPrevented) {
+      node.dispatchEvent(new CustomEvent('click_outside', node))
+    }
+  };
+	document.addEventListener('click', handleClick, true);
+  return {
+    destroy() {
+      document.removeEventListener('click', handleClick, true);
+    }
+	}
+}
+
+// Focus on mount
+export function focusOnMount(node) {
+  node.focus();
+}
+
+// API response headers
+export const apiRsponseHeaders = {
+  'content-type': 'application/json; charset=utf-8',
+};
+
+// API response status
+export const apiResponseStatus = function (data) {
+  if (data && data.length > 0) {
+    // OK
+    return 200;
+  } else if (data && data.length === 0) {
+    // NOT FOUND
+    return 404;
+  } else if (!data) {
+    // INTERNAL SERVER ERROR
+    return 500;
+  };
+}
+
+export const numSpace = function (num) {
+  return num.toString().length > 4 ? num.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1 ') : num.toString();
+};
+
+export const yearFrom = function (year) {
+	return new Date().getFullYear() > year ? year + ' — ' + new Date().getFullYear() : year;
+};
+
+export const addZero = function (num) {
+  return num.toString().length < 2 ? '0' + num.toString() : num.toString();
+};
+
+export const formatPhone = function (phone) {
+  return phone.replace(/(\d{1})(\d{3})(\d{3})(\d{2})(\d{2})/, "$1 $2 $3 $4 $5");
+};
+
+export const formatPhoneLink = function (str) {
+  str = str.replace(/<.+>/, ' ').replace(/\s+/, ' ');
+	let c = {
+		'-':'', '(':'', ')':'', ' ':''
+	};
+	let newStr = new String();
+	for (let i = 0; i < str.length; i++) {
+		let ch = str.charAt(i);
+		newStr += ch in c ? c[ch] : ch;
+	};
+	return newStr;
+};
+
+export const getStarsText = (stars) => {
+  let startsText = '';
+  while (stars-- > 0) startsText += '*';
+  return startsText;
+};
+
+export const arrayRandElement = function(arr) {
+  var rand = Math.floor(Math.random() * arr.length);
+  return arr[rand];
+};
