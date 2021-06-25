3ba86f5d9814bf254e526c03c1cb44165605e33b
Den Kochetkov <d@zdp.su>
2021-06-25 13:05:04 +0300

    Items added to catalog

--- src/routes/shop/_catalog.js
+++ src/routes/shop/_catalog.js
@@ -117,6 +117,122 @@ const catalogItems = [
       </p>
     `,
   },
+  {
+    name: 'Achilllea',
+    photo: '/media/bag06.jpg',
+    slug: toSlug('Achilllea'),
+    price: 372,
+    description: `
+      <p>
+        Small woman handbag in raffia fabric, handmade on ancient loom. Compact in size, equipped with magnetic closure, it is capacious enough to carry with you the essential. Its minimal shape is enriched by the interesting and lively interlacement of the fabric, combined with the round leather handle made with the macramé technique. Entirely designed and made in Italy.
+      </p>
+      <p>
+        Material: 100% viscose raffia
+      </p>
+      <p>
+        Processing: Handwoven
+      </p>
+      <p>
+        Lining: 100% cotton with patch pocket
+      </p>
+      <p>
+        Handle: 100% leather
+      </p>
+      <p>
+        Made in Italy
+      </p>
+      <p>
+        Width 23 cm Height 17 cm Depth 5 cm
+      </p>
+    `,
+  },
+  {
+    name: 'Poppy',
+    photo: '/media/bag14.jpg',
+    slug: toSlug('Poppy'),
+    price: 473,
+    description: `
+      <p>
+        Small woman handbag in raffia fabric, handmade on ancient loom. Compact in size, equipped with magnetic closure, it is capacious enough to carry with you the essential. Its minimal shape is enriched by the interesting and lively interlacement of the fabric, combined with the round leather handle made with the macramé technique. Entirely designed and made in Italy.
+      </p>
+      <p>
+        Material: 100% viscose raffia
+      </p>
+      <p>
+        Processing: Handwoven
+      </p>
+      <p>
+        Lining: 100% cotton with patch pocket
+      </p>
+      <p>
+        Handle: 100% leather
+      </p>
+      <p>
+        Made in Italy
+      </p>
+      <p>
+        Width 23 cm Height 17 cm Depth 5 cm
+      </p>
+    `,
+  },
+  {
+    name: 'Stromboli',
+    photo: '/media/bag12.jpg',
+    slug: toSlug('Stromboli'),
+    price: 120,
+    description: `
+      <p>
+        Small woman handbag in raffia fabric, handmade on ancient loom. Compact in size, equipped with magnetic closure, it is capacious enough to carry with you the essential. Its minimal shape is enriched by the interesting and lively interlacement of the fabric, combined with the round leather handle made with the macramé technique. Entirely designed and made in Italy.
+      </p>
+      <p>
+        Material: 100% viscose raffia
+      </p>
+      <p>
+        Processing: Handwoven
+      </p>
+      <p>
+        Lining: 100% cotton with patch pocket
+      </p>
+      <p>
+        Handle: 100% leather
+      </p>
+      <p>
+        Made in Italy
+      </p>
+      <p>
+        Width 23 cm Height 17 cm Depth 5 cm
+      </p>
+    `,
+  },
+  {
+    name: 'Sorbonne',
+    photo: '/media/bag17.jpg',
+    slug: toSlug('Sorbonne'),
+    price: 580,
+    description: `
+      <p>
+        Small woman handbag in raffia fabric, handmade on ancient loom. Compact in size, equipped with magnetic closure, it is capacious enough to carry with you the essential. Its minimal shape is enriched by the interesting and lively interlacement of the fabric, combined with the round leather handle made with the macramé technique. Entirely designed and made in Italy.
+      </p>
+      <p>
+        Material: 100% viscose raffia
+      </p>
+      <p>
+        Processing: Handwoven
+      </p>
+      <p>
+        Lining: 100% cotton with patch pocket
+      </p>
+      <p>
+        Handle: 100% leather
+      </p>
+      <p>
+        Made in Italy
+      </p>
+      <p>
+        Width 23 cm Height 17 cm Depth 5 cm
+      </p>
+    `,
+  },
 ];
 
 export { catalogItems };
