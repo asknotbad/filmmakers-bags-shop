8727939a87667c12aa9b3203c065954ebe2f2dbc
Den Kochetkov <d@zdp.su>
2021-06-25 12:14:07 +0300

    Cart layout done

--- src/lib/Cart/index.svelte
+++ src/lib/Cart/index.svelte
@@ -1,5 +1,29 @@
 <script>
+  import { numSpace } from '$lib/helpers';
+  import ItemInCart from './ItemInCart.svelte';
   import { itemsInCart } from "$lib/stores";
+
+  let items = $itemsInCart.filter((item, index, self) =>
+    index === self.findIndex((el) => (
+      el.slug === item.slug
+    ))
+  );
+
+  items.map((item, i) => {
+    let count = 0;
+    $itemsInCart.map(el => {
+      if (el.slug === item.slug) {
+        count++;
+      };
+    });
+    items[i].count = count;
+  });
+
+  let subTotal = 0;
+
+  $itemsInCart.map(el => {
+    subTotal += el.price;
+  });
 </script>
 
 <section>
@@ -14,6 +38,15 @@
             <p class="empty">
               Your shopping cart is empty.
             </p>
+          {:else}
+            <div class="header">
+              Product
+            </div>
+            <div class="items">
+              {#each items as item}
+                <ItemInCart {item} />
+              {/each}
+            </div>
           {/if}
         </div>
         <div class="continue">
@@ -21,6 +54,20 @@
             Continue shopping
           </a>
         </div>
+        {#if $itemsInCart.length > 0}
+          <div class="header subtotal">
+            Subtotal
+          </div>
+          <div class="price">
+            € {numSpace(subTotal)}
+          </div>
+
+          <div class="continue">
+            <a class="button" href="/cart/checkout">
+              Proceed to checkout
+            </a>
+          </div>
+        {/if}
       </div>
     </div>
   </div>
@@ -61,5 +108,32 @@
     cursor: pointer;
     letter-spacing: .1em;
     text-decoration: none;
+  }
+  .header {
+    padding: 8px 16px;
+    text-transform: uppercase;
+    background: #f4f3f9;
+    font-weight: 700;
+    font-size: 14px;
+    color: #000;
+    margin-bottom: 24px;
+  }
+  .subtotal {
+    text-align: center;
+    margin-bottom: 32px;
+  }
+  .items {
+    display: grid;
+    grid-template-columns: 1fr;
+    gap: 32px;
+  }
+  .continue {
+    margin-bottom: 32px;
+  }
+  .price {
+    text-align: center;
+    color: #00406F;
+    font-weight: 700;
+    font-size: 32px;
   }
 </style>
